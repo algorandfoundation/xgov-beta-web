00c6d41d6225a6e92ce4f1ebb4750e3be9ec093f
Kyle Breeding <kyle.breeding@algorand.foundation>
2024-12-23 10:43:02 -0800

    chore: added react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
     QueryClient,
     QueryClientProvider,
 } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { HomePage } from "@/pages/_HomePage";
 import { ProposalPage } from "@/pages/proposal/_ProposalPage";
 import { DocsPage } from "@/pages/_DocsPage";
@@ -54,6 +55,7 @@ export function App({ path = "/" }) {
     return (
         <React.StrictMode>
             <QueryClientProvider client={queryClient}>
+                <ReactQueryDevtools />
                 <WalletProvider manager={walletManager}>
                     <RouterProvider router={router} />
                 </WalletProvider>
