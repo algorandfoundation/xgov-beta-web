5f3627f6436adaf882bba024c127bfbc4db3b88e
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2025-01-31 14:40:32 +0300

    fix: switch to advanced mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
-import react from "@astrojs/react";
+import react from "@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: cloudflare({
+    mode: 'advanced',
+  }),
   vite: {
     define: {
       global: 'globalThis',
     },
   },
-  adapter: cloudflare({ mode: "directory" }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
