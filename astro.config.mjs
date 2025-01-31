2e1e58dcdc8b912bc401fec5ae9ab3b764b77351
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2025-01-31 14:42:58 +0300

    fix: string literal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
-import react from "@astrojs/react';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
