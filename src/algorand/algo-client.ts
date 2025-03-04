c7c59622e38db3215b77f92fe064f62ebfb0bdc4
Michael Feher <michael.feher@algorand.foundation>
2025-03-04 13:44:31 -0500

    chore(algorand): set defaults for clients

--- src/algorand/algo-client.ts
+++ src/algorand/algo-client.ts
@@ -3,22 +3,22 @@ import algosdk from 'algosdk'
 import { ClientManager } from '@algorandfoundation/algokit-utils/types/client-manager'
 
 export const indexer = ClientManager.getIndexerClient({
-    server: import.meta.env.PUBLIC_INDEXER_SERVER,
-    port: import.meta.env.PUBLIC_INDEXER_PORT,
-    token: import.meta.env.PUBLIC_INDEXER_TOKEN,
+    server: import.meta.env.PUBLIC_INDEXER_SERVER || 'http://localhost',
+    port: import.meta.env.PUBLIC_INDEXER_PORT || '4002',
+    token: import.meta.env.PUBLIC_INDEXER_TOKEN || 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
 });
 
 export const algod = ClientManager.getAlgodClient({
-    server: import.meta.env.PUBLIC_ALGOD_SERVER,
-    port: import.meta.env.PUBLIC_ALGOD_PORT,
-    token: import.meta.env.PUBLIC_ALGOD_TOKEN,
+    server: import.meta.env.PUBLIC_ALGOD_SERVER || 'http://localhost',
+    port: import.meta.env.PUBLIC_ALGOD_PORT || '4001',
+    token: import.meta.env.PUBLIC_ALGOD_TOKEN || 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
 });
 
 export const kmd: algosdk.Kmd | undefined = import.meta.env.PUBLIC_NETWORK === 'LocalNet'
     ? ClientManager.getKmdClient({
-        server: import.meta.env.PUBLIC_KMD_SERVER,
-        port: import.meta.env.PUBLIC_KMD_PORT,
-        token: import.meta.env.PUBLIC_ALGOD_TOKEN,
+        server: import.meta.env.PUBLIC_KMD_SERVER || 'http://localhost',
+        port: import.meta.env.PUBLIC_KMD_PORT || '4002',
+        token: import.meta.env.PUBLIC_ALGOD_TOKEN || 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
     })
     : undefined;
 
