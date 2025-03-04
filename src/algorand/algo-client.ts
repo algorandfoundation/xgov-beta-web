804025c0dc8481e185597b4530ba8e68a384c751
Michael Feher <michael.feher@algorand.foundation>
2025-03-04 16:38:16 -0500

    chore(client): clean up initialization, stage for using init script on FNET/Testnet

--- src/algorand/algo-client.ts
+++ src/algorand/algo-client.ts
@@ -1,24 +1,26 @@
 import { AlgorandClient as AC } from '@algorandfoundation/algokit-utils'
 import algosdk from 'algosdk'
 import { ClientManager } from '@algorandfoundation/algokit-utils/types/client-manager'
+import {env} from '@/constants'
 
+console.log("Loading AlgorandClient")
 export const indexer = ClientManager.getIndexerClient({
-    server: import.meta.env.PUBLIC_INDEXER_SERVER || 'https://fnet-idx.4160.nodely.dev',
-    port: import.meta.env.PUBLIC_INDEXER_PORT || '443',
-    token: import.meta.env.PUBLIC_INDEXER_TOKEN || '',
+    server: env.PUBLIC_INDEXER_SERVER || 'https://fnet-idx.4160.nodely.dev',
+    port: env.PUBLIC_INDEXER_PORT || '443',
+    token: env.PUBLIC_INDEXER_TOKEN || '',
 });
 
 export const algod = ClientManager.getAlgodClient({
-    server: import.meta.env.PUBLIC_ALGOD_SERVER || 'https://fnet-api.4160.nodely.dev',
-    port: import.meta.env.PUBLIC_ALGOD_PORT || '443',
-    token: import.meta.env.PUBLIC_ALGOD_TOKEN || '',
+    server: env.PUBLIC_ALGOD_SERVER || 'https://fnet-api.4160.nodely.dev',
+    port: env.PUBLIC_ALGOD_PORT || '443',
+    token: env.PUBLIC_ALGOD_TOKEN || '',
 });
 
-export const kmd: algosdk.Kmd | undefined = import.meta.env.PUBLIC_NETWORK === 'LocalNet'
+export const kmd: algosdk.Kmd | undefined = env.PUBLIC_NETWORK === 'localnet'
     ? ClientManager.getKmdClient({
-        server: import.meta.env.PUBLIC_KMD_SERVER || 'http://localhost',
-        port: import.meta.env.PUBLIC_KMD_PORT || '4002',
-        token: import.meta.env.PUBLIC_ALGOD_TOKEN || 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        server: env.PUBLIC_KMD_SERVER || 'http://localhost',
+        port: env.PUBLIC_KMD_PORT || '4002',
+        token: env.PUBLIC_ALGOD_TOKEN || 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
     })
     : undefined;
 
