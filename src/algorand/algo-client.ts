58197c79c60eb3a4f3ecee43819d1a1e56b20c23
Michael Feher <michael.feher@algorand.foundation>
2025-03-04 14:51:48 -0500

    chore(algorand): use fnet for defaults

--- src/algorand/algo-client.ts
+++ src/algorand/algo-client.ts
@@ -3,15 +3,15 @@ import algosdk from 'algosdk'
 import { ClientManager } from '@algorandfoundation/algokit-utils/types/client-manager'
 
 export const indexer = ClientManager.getIndexerClient({
-    server: import.meta.env.PUBLIC_INDEXER_SERVER || 'http://localhost',
-    port: import.meta.env.PUBLIC_INDEXER_PORT || '4002',
-    token: import.meta.env.PUBLIC_INDEXER_TOKEN || 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    server: import.meta.env.PUBLIC_INDEXER_SERVER || 'https://fnet-idx.4160.nodely.dev',
+    port: import.meta.env.PUBLIC_INDEXER_PORT || '443',
+    token: import.meta.env.PUBLIC_INDEXER_TOKEN || '',
 });
 
 export const algod = ClientManager.getAlgodClient({
-    server: import.meta.env.PUBLIC_ALGOD_SERVER || 'http://localhost',
-    port: import.meta.env.PUBLIC_ALGOD_PORT || '4001',
-    token: import.meta.env.PUBLIC_ALGOD_TOKEN || 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    server: import.meta.env.PUBLIC_ALGOD_SERVER || 'https://fnet-api.4160.nodely.dev',
+    port: import.meta.env.PUBLIC_ALGOD_PORT || '443',
+    token: import.meta.env.PUBLIC_ALGOD_TOKEN || '',
 });
 
 export const kmd: algosdk.Kmd | undefined = import.meta.env.PUBLIC_NETWORK === 'LocalNet'
