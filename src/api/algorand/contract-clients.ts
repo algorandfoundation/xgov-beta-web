222761ae29840c0923b6b1c8027d0bdbe70c5dab
Tasos Bitsios <tasos.bitsios@algorand.foundation>
2025-06-20 17:23:26 +0300

    chore: use private backend client in api/assign

--- src/api/algorand/contract-clients.ts
+++ src/api/algorand/contract-clients.ts
@@ -4,17 +4,28 @@ import {
 } from "@algorandfoundation/xgov/registry";
 import { ProposalClient as XGovProposalClient } from "@algorandfoundation/xgov/proposal";
 import { algorand } from "./algo-client";
-import {env} from '@/constants'
+import { env } from "@/constants";
+import type { AlgorandClient } from "@algorandfoundation/algokit-utils";
 
 const DEFAULT_REGISTRY_APP_ID = 16324508;
 export const RegistryAppID: bigint = BigInt(
-  env.PUBLIC_REGISTRY_APP_ID !== '<REPLACE_WITH_APPLICATION_ID>' ? env.PUBLIC_REGISTRY_APP_ID || DEFAULT_REGISTRY_APP_ID : DEFAULT_REGISTRY_APP_ID,
+  env.PUBLIC_REGISTRY_APP_ID !== "<REPLACE_WITH_APPLICATION_ID>"
+    ? env.PUBLIC_REGISTRY_APP_ID || DEFAULT_REGISTRY_APP_ID
+    : DEFAULT_REGISTRY_APP_ID,
 );
 
 export const registryClient = algorand.client.getTypedAppClientById(
   XGovRegistryClient,
   { appId: RegistryAppID },
 );
+
+export function getRegistryClient(
+  algorand: AlgorandClient,
+): XGovRegistryClient {
+  return algorand.client.getTypedAppClientById(XGovRegistryClient, {
+    appId: RegistryAppID,
+  });
+}
 
 // TODO: make prototype of the ProposalClient
 registryClient.getState = async function (): Promise<TypedGlobalState> {
--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -14,3 +14,23 @@ export function chunk<T>(arr: T[], chunkSize: number): T[][] {
     }
     return result;
 }
+
+export function getNumericEnvironmentVariable(
+  localsKey: string,
+  locals: App.Locals,
+  defaultValue: number,
+): number {
+  // @ts-expect-error, runtime can be undefined
+  return locals?.runtime?.env && localsKey in locals?.runtime?.env
+    ? // @ts-expect-error, runtime can be undefined
+      parseInt(locals.runtime.env[localsKey], 10)
+    : defaultValue;
+}
+
+export function getStringEnvironmentVariable(key: string, locals: App.Locals, defaultValue: string): string {
+  // @ts-expect-error, this can be undefined
+  if (locals?.runtime?.env && key in locals?.runtime?.env)
+    // @ts-expect-error, this can be undefined
+    return locals?.runtime?.env[key];
+  return import.meta.env[key] ?? defaultValue;
+}
