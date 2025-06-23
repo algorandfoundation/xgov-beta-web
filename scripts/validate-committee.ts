dc91ad5abc1fbe0f1d0b9b48b7e87a87e40b72b9
Tasos Bitsios <tasos.bitsios@algorand.foundation>
2025-06-23 14:29:43 +0300

    chore: committee validator & ID calculator utility

---
+++ scripts/validate-committee.ts
@@ -0,0 +1,50 @@
+import { sha512_256 } from "js-sha512";
+import { readFileSync } from "fs";
+import Ajv, { JSONSchemaType } from "ajv";
+import { deepEqual } from "assert";
+import { isDeepStrictEqual } from "util";
+
+// copied from api/assign.ts
+function committeeIdToSafeFileName(committeeId: string): string {
+  // Use base64url encoding (base64 without padding, using URL-safe characters)
+  return committeeId
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
+
+const filename = process.argv[2];
+
+if (!filename) throw new Error("No filename provided");
+
+const contents = readFileSync(filename);
+
+const jsonContents = JSON.parse(contents.toString());
+const schema = JSON.parse(readFileSync("./committee-schema.json").toString());
+
+const ajv = new Ajv();
+const validate = ajv.compile(schema);
+
+const valid = validate(jsonContents);
+if (valid) {
+  console.log("Schema validation OK");
+} else {
+  console.warn("Schema validation errors:");
+  console.warn(validate.errors);
+}
+
+const actualSort = jsonContents.xGovs.map(({address: a}) => a)
+const expectedSort = [...actualSort].sort()
+
+if (!isDeepStrictEqual(actualSort, expectedSort)) {
+  console.warn("Validation error: xGov array not sorted!")
+} else {
+  console.warn("xGov array is sorted")
+}
+
+const fileHash = Buffer.from(sha512_256(contents), "hex");
+const concatenated = Buffer.concat([Buffer.from("arc0034"), fileHash]);
+const committeeId = Buffer.from(sha512_256(concatenated), "hex").toString("base64");
+
+console.log("Committee ID:", committeeId)
+console.log("Safe mode:", committeeIdToSafeFileName(committeeId))
