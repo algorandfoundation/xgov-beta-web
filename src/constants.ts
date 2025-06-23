1a4a1cfb7a2f6f1035f6c6a171a286f5d73c7462
Kyle Breeding <kyle.breeding@algorand.foundation>
2025-06-23 09:13:04 -0700

    fix: localnet default registry values

--- src/constants.ts
+++ src/constants.ts
@@ -5,8 +5,8 @@ export const FEE_SINK = 'A7NMWS3NT3IUDMLVO26ULGXGIIOUQ3ND2TXSER6EBGRZNOBOUIQXHIB
 export const XGOV_FEE = BigInt(1_000_000);
 export const PROPOSER_FEE = BigInt(10_000_000);
 export const PROPOSAL_FEE = BigInt(100_000_000);
-export const PROPOSAL_PUBLISHING_BPS = BigInt(1_000);
-export const PROPOSAL_COMMITMENT_BPS = BigInt(1_000);
+export const PROPOSAL_PUBLISHING_BPS = BigInt(500);
+export const PROPOSAL_COMMITMENT_BPS = BigInt(300);
 export const MIN_REQUESTED_AMOUNT = BigInt(1_000);
 
 export const MAX_REQUESTED_AMOUNT_SMALL = BigInt(100_000_000_000);
@@ -23,10 +23,10 @@ export const VOTING_DURATION_MEDIUM = BigInt(172800);
 export const VOTING_DURATION_LARGE = BigInt(259200);
 export const VOTING_DURATION_XLARGE = BigInt(345600);
 
-export const QUORUM_SMALL = BigInt(100);
-export const QUORUM_MEDIUM = BigInt(200);
-export const QUORUM_LARGE = BigInt(300);
+export const QUORUM_SMALL = BigInt(1_000);
+export const QUORUM_MEDIUM = BigInt(1_500);
+export const QUORUM_LARGE = BigInt(2_000);
 
-export const WEIGHTED_QUORUM_SMALL = BigInt(200);
-export const WEIGHTED_QUORUM_MEDIUM = BigInt(300);
-export const WEIGHTED_QUORUM_LARGE = BigInt(400);
+export const WEIGHTED_QUORUM_SMALL = BigInt(2_000);
+export const WEIGHTED_QUORUM_MEDIUM = BigInt(3_000);
+export const WEIGHTED_QUORUM_LARGE = BigInt(4_000);
