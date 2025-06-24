9e94a1b1b74f286b15deb92a9d0a3fa3b15a5c85
Tasos Bitsios <tasos.bitsios@algorand.foundation>
2025-06-25 00:44:47 +0300

    chore: testnet-funder logic sig change for new testnet registry 741784511

--- src/api/testnet-funding-logicsig.ts
+++ src/api/testnet-funding-logicsig.ts
@@ -8,7 +8,7 @@ import { LogicSigAccount, makeLogicSigAccountTransactionSigner } from "algosdk";
 
 // https://github.com/algorandfoundation/xgov-testnet-proposers-funding-logicsig
 
-const fundingLsigB64 = "BSACAYDC1y8zAACAIL3M6t8UQ06GsE/Jx/JpN1pM/Maqx0Hds0TPhgv71fW6EkAApjIEgQMSMSAyAxIQMQkyAxIQMQEyABIQMwAQIhIQMwAHMwEAEhAzAAczAgASEDMACCMSEDMBECISEDMBB4Ag+jRuARjdHJ6Tavjzdvb4Zeq2d1iKhSJVQiZKZMQg0skSEDMBCCMSEDMCEIEGEhAzAhiBlfms4QISEDcCGgCABHpP7kMSNwIaAIAEoILO+BIREDMCGYEAEhCACN+VqwARil/cFxBCAAEiQw=="
+const fundingLsigB64 = "BSACAYDC1y8zAACAIL3M6t8UQ06GsE/Jx/JpN1pM/Maqx0Hds0TPhgv71fW6EkAApjIEgQMSMSAyAxIQMQkyAxIQMQEyABIQMwAQIhIQMwAHMwEAEhAzAAczAgASEDMACCMSEDMBECISEDMBB4Ag+jRuARjdHJ6Tavjzdvb4Zeq2d1iKhSJVQiZKZMQg0skSEDMBCCMSEDMCEIEGEhAzAhiBv/fa4QISEDcCGgCABHpP7kMSNwIaAIAEoILO+BIREDMCGYEAEhCACAdM1AABcu6dFxBCAAEiQw=="
 
 export const fundingLogicSig = new LogicSigAccount(Buffer.from(fundingLsigB64, "base64"));
 
