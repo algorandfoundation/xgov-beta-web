09b56c9371cc594a495986ba04f23b5aabd8cd03
Kyle Breeding <kyle.breeding@algorand.foundation>
2025-07-02 22:22:17 -0700

    chore: testnet support for nfd api fetching

--- src/pages/api/nfd/[query].ts
+++ src/pages/api/nfd/[query].ts
@@ -1,7 +1,8 @@
+import { BASE_NFD_API_URL } from "@/api";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ params }) => {
-  const r = await fetch(`https://api.nf.domains/${params.query}`)
+  const r = await fetch(`${BASE_NFD_API_URL}/${params.query}`)
   if(r.status === 404){
     return new Response(null, {
       status: 404,
