9b67766f79c1d34b5c14f3fd88a593fdb0f5d56c
Tasos Bitsios <tasos.bitsios@algorand.foundation>
2026-02-10 12:48:34 +0100

    chore: remove ts-expect-error from utils/getStringEnvironmentVariable

--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -6,28 +6,36 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function chunk<T>(arr: T[], chunkSize: number): T[][] {
-    if (chunkSize <= 0) throw new Error("chunkSize must be greater than 0");
+  if (chunkSize <= 0) throw new Error("chunkSize must be greater than 0");
 
-    const result: T[][] = [];
-    for (let i = 0; i < arr.length; i += chunkSize) {
-        result.push(arr.slice(i, i + chunkSize));
+  const result: T[][] = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    result.push(arr.slice(i, i + chunkSize));
+  }
+  return result;
+}
+
+export function getNumericEnvironmentVariable(
+  key: string,
+  locals: App.Locals,
+  defaultValue: number,
+): number {
+  return parseInt(
+    getStringEnvironmentVariable(key, locals, defaultValue.toString()),
+    10,
+  );
+}
+
+export function getStringEnvironmentVariable(
+  key: string,
+  locals: App.Locals,
+  defaultValue: string,
+): string {
+  if ("runtime" in locals && locals.runtime) {
+    const { env } = (locals.runtime as any);
+    if (key in env) {
+      return env[key];
     }
-    return result;
-}
-
-export function getNumericEnvironmentVariable(key: string, locals: App.Locals, defaultValue: number): number {
-  // @ts-expect-error, runtime can be undefined
-  if (locals?.runtime?.env && key in locals?.runtime?.env)
-    // @ts-expect-error, runtime can be undefined
-    return parseInt(locals.runtime.env[key], 10);
-  const envValue = import.meta.env[key];
-  return envValue !== undefined ? parseInt(envValue, 10) : defaultValue;
-}
-
-export function getStringEnvironmentVariable(key: string, locals: App.Locals, defaultValue: string): string {
-  // @ts-expect-error, this can be undefined
-  if (locals?.runtime?.env && key in locals?.runtime?.env)
-    // @ts-expect-error, this can be undefined
-    return locals?.runtime?.env[key];
+  }
   return import.meta.env[key] ?? defaultValue;
 }
